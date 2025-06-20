58773ed73f9918f63acfb87d2a3bad0fdac2e54d
m4tice <ductuan2908@gmail.com>
2025-06-20 23:49:42 +0700

    feat: rq1 v2 first development

--- app/rq1/static/v2.js
+++ app/rq1/static/v2.js
@@ -3,5 +3,43 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function main(){
-    // Main function to initialize the page
+    onButtonClick();
+}
+
+function onButtonClick(){
+    const buttonLightMode = document.getElementById('buttonLightMode');
+    const buttonDarkMode = document.getElementById('buttonDarkMode');
+    const buttonSetting = document.getElementById('buttonSetting');
+    const buttonDebug = document.getElementById('buttonDebug');
+
+    buttons = [buttonLightMode, buttonDarkMode, buttonDebug, buttonSetting];
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            console.log("[DEBUG] Button '" + button.id.split("button")[1] + "' clicked.");
+        });
+    });
+}
+
+function createButtons(){
+
+}
+
+function createButton(buttonText){
+
+}
+
+function createTable(){
+
+}
+
+function createHeaders(){
+
+}
+
+function createHeader(headerText){
+
+}
+
+function createRows(data){
+    
 }
